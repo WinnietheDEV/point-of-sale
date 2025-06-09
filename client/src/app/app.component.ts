bd641656928947e7bdee224564f4f95d13adcaea
nawin <nawin.dev@no-tus.com>
2025-06-09 16:35:43 +0700

    client/feat: import products component เข้าสู่ app.component.ts และแสดงใน app.component.html

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProductsComponent } from './features/products/products.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ProductsComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'client';
