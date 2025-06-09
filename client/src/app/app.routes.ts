14f8eab1df73c8332f5d05fb54a57392a7db5a03
nawin <nawin.dev@no-tus.com>
2025-06-10 00:40:10 +0700

    client/feat: เขียน app.routes เพื่อกำหนด component ที่ user จะเห็นเมื่อไป path ต่างๆ, สร้าง not-found component

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ProductsComponent } from './features/products/products.component';
+import { NotFoundComponent } from './features/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'products', pathMatch: 'full' },
+  { path: 'products', component: ProductsComponent },
+  { path: '**', component: NotFoundComponent },
+];
