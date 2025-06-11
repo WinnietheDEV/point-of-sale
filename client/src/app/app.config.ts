6999ec39450e4ac7abaa546c78fa4472595e60fd
nawin <nawin.dev@no-tus.com>
2025-06-11 16:16:52 +0700

    client/delete: ลบ product component เนื่องจากตัดฟีเจอร์ดูรายละเอียดสินค้าออก

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,5 +1,9 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {
+  provideRouter,
+  withComponentInputBinding,
+  withRouterConfig,
+} from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
@@ -9,5 +13,12 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(),
+    provideRouter(
+      routes,
+      withComponentInputBinding(),
+      withRouterConfig({
+        paramsInheritanceStrategy: 'always',
+      })
+    ),
   ],
 };
--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,13 +1,11 @@
 import { Routes } from '@angular/router';
 import { ProductsComponent } from './features/products/products.component';
 import { NotFoundComponent } from './features/not-found/not-found.component';
-import { ProductComponent } from './features/product/product.component';
 import { CartComponent } from './features/cart/cart.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'products', pathMatch: 'full' },
   { path: 'products', component: ProductsComponent },
-  { path: 'products/:id', component: ProductComponent },
   { path: 'carts', component: CartComponent },
   { path: '**', component: NotFoundComponent },
 ];
