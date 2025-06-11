24af7a1ffe0594ab369d9c77b9ba600fabf51606
nawin <nawin.dev@no-tus.com>
2025-06-11 23:36:54 +0700

    client/feat: ใส่ ยอดรวม, ส่วนลด, ยอดสุทธิ ใน checkout component, ตกแต่ง css, ใช้ formatMoneyInput กับ discount input

--- client/src/app/features/checkout/checkout.component.ts
+++ client/src/app/features/checkout/checkout.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { formatMoneyInput } from '../../utils/number-format.util';
 
 @Component({
   selector: 'app-checkout',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './checkout.component.html',
-  styleUrl: './checkout.component.css'
+  styleUrl: './checkout.component.css',
 })
 export class CheckoutComponent {
+  discount = '0';
 
+  onDiscountInput(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    const formatted = formatMoneyInput(input.value, this.discount);
+    input.value = formatted;
+    this.discount = formatted;
+  }
 }
