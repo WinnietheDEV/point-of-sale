445792b113a62c0997fe44cca901dfb5f5b79ce3
nawin <nawin.dev@no-tus.com>
2025-06-12 09:46:48 +0700

    client/feat: ดึง cartItems จาก cartService มาแสดงรายการสินค้าในตะกร้า

--- client/src/app/features/cart/cart.component.ts
+++ client/src/app/features/cart/cart.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CartService } from './service/cart.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-cart',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './cart.component.html',
-  styleUrl: './cart.component.css'
+  styleUrl: './cart.component.css',
 })
 export class CartComponent {
-
+  private cartService = inject(CartService);
+  readonly itemsInCart = this.cartService.cartItems;
 }
