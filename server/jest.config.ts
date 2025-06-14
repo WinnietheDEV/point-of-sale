a7befb078ea1ebbd644b39454bfc6df7d86d403d
nawin <nawin.dev@no-tus.com>
2025-06-15 00:03:10 +0700

    server/chore: สร้าง jest.config สำหรับเทส server

---
+++ server/jest.config.ts
@@ -0,0 +1,17 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: '.',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    '^src/(.*)$': '<rootDir>/src/$1', // 👈 fix for absolute imports like src/...
+  },
+  coverageDirectory: './coverage',
+  testEnvironment: 'node',
+};
+
+export default config;
