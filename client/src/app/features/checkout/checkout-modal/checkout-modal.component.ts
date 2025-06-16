dd0c992369cfe81460669ccde61810a7f9c50b9e
nawin <nawin.dev.th@gmail.com>
2025-06-16 15:24:04 +0700

    client/feat: สร้าง checkout-modal component

---
+++ client/src/app/features/checkout/checkout-modal/checkout-modal.component.ts
@@ -0,0 +1,33 @@
+import { Component, inject, Input, OnInit, signal } from '@angular/core';
+import { CheckoutService } from '../service/checkout.service';
+import { CartService } from '../../cart/service/cart.service';
+import { CurrencyPipe } from '@angular/common';
+
+@Component({
+  selector: 'app-checkout-modal',
+  imports: [CurrencyPipe],
+  templateUrl: './checkout-modal.component.html',
+  styleUrl: './checkout-modal.component.css',
+})
+export class CheckoutModalComponent implements OnInit {
+  paymentMethod = signal<string>('เงินสด');
+  @Input() grandTotal: number = 0;
+  pay: number = 0;
+  change = signal<number>(0);
+  private checkoutService = inject(CheckoutService);
+  private cartService = inject(CartService);
+
+  ngOnInit() {
+    this.pay = this.grandTotal;
+  }
+
+  onMakeTransaction() {
+    this.checkoutService.makeTransaction({
+      products: this.cartService.cartItems(),
+      grandTotal: this.grandTotal,
+      pay: this.pay,
+      change: this.change(),
+      paymentMethod: this.paymentMethod(),
+    });
+  }
+}
