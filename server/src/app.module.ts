320c541adbeac7588a766dab0da49129869783b4
nawin <nawin.dev.th@gmail.com>
2025-06-16 15:17:21 +0700

    server/chore: เพิ่ม validator และ transformer package, import TransactionModule สู่ AppModule

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,12 +4,14 @@ import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ProductsModule } from './product/product.module';
 import { ConfigModule } from '@nestjs/config';
+import { TransactionModule } from './transaction/transaction.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.MONGO_URL!),
     ProductsModule,
+    TransactionModule,
   ],
   controllers: [AppController],
   providers: [AppService],
